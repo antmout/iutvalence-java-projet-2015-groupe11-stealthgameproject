459b9bdf26880250cceb849b066a516c94370893
antmout <antoine@Pollux>
2015-04-29 22:48:51 +0200

    Creation of the GameLauncher.

---
+++ src/fr/iutvalence/ardechois/stealthgameproject/GameLauncher.java
@@ -0,0 +1,16 @@
+package fr.iutvalence.ardechois.stealthgameproject;
+
+/**
+ * Allow to launch the application.
+ * 
+ * @author antoine
+ * @version 0.1.0
+ */
+public class GameLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
