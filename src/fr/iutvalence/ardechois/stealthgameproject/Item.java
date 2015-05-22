ae6762e66c22406a51a73ea061482127cf51b520
MaximeVan <maxime.vanbossel@hotmail.fr>
2015-05-22 10:57:53 +0200

    Changes in class item, add of a getter Taken, and javadoc.

--- src/fr/iutvalence/ardechois/stealthgameproject/Item.java
+++ src/fr/iutvalence/ardechois/stealthgameproject/Item.java
@@ -8,22 +8,53 @@ package fr.iutvalence.ardechois.stealthgameproject;
  * */
 public class Item
 {
-	/** The Item Position. */
-	private Position position;
+	/**
+	 * The Item Position.
+	 * 
+	 * @see Position
+	 * 
+	 * */
+	private static Position position;
+
+	/**
+	 * True if the Item is already taken by the Character.
+	 * 
+	 * @see Character
+	 * 
+	 */
+	private boolean taken;
 
 	/**
 	 * Constructor of Item Position
 	 * 
 	 * @param position
+	 * 
 	 */
 	public Item(Position position)
 	{
 		this.position = position;
 	}
-	
+
+	/**
+	 * Getter for the Item Position.
+	 * 
+	 * @return position
+	 * 
+	 */
 	public Position getPosition()
 	{
 		return position;
 	}
-	
+
+	/**
+	 * Getter Taken.
+	 * 
+	 * @return taken
+	 * 
+	 */
+	public boolean isTaken()
+	{
+		return taken;
+	}
+
 }
