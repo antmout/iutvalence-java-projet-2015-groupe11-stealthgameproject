a4e23c5e5644f874b0076107d602a75382ac28ef
kelemen <nicolas.kelemen0@gmail.com>
2015-05-22 10:47:06 +0200

    Starting of the Character class.

--- src/fr/iutvalence/ardechois/stealthgameproject/Character.java
+++ src/fr/iutvalence/ardechois/stealthgameproject/Character.java
@@ -2,5 +2,11 @@ package fr.iutvalence.ardechois.stealthgameproject;
 
 public class Character
 {
-
+	/** The character position */
+	private Position position;
+	
+	public Character(Position position)
+	{
+		this.position = position;
+	}
 }
