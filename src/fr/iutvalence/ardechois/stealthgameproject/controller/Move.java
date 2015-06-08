b62c52774d426f345c73d00c17123762ff60d383
moutona <moutona@PC-B124-03.iut-valence.fr>
2015-06-08 11:42:25 +0200

    Modification of Direction

---
+++ src/fr/iutvalence/ardechois/stealthgameproject/controller/Move.java
@@ -0,0 +1,8 @@
+package fr.iutvalence.ardechois.stealthgameproject.controller;
+
+import fr.iutvalence.ardechois.stealthgameproject.model.Position;
+
+public class Move
+{
+
+}
--- src/fr/iutvalence/ardechois/stealthgameproject/model/Direction.java
+++ src/fr/iutvalence/ardechois/stealthgameproject/model/Direction.java
@@ -1,9 +1,66 @@
 package fr.iutvalence.ardechois.stealthgameproject.model;
 
 public enum Direction
+
 {
-	UP,
-	DOWN,
-	RIGHT,
-	LEFT;
+    /**
+     * .
+     * 
+     * @value "0,1"
+     */
+    UP(0, 1),
+
+    /**
+     * .
+     * 
+     * @value "0,-1"
+     */
+    DOWN(0, -1),
+
+    /**
+     * .
+     * 
+     * @value "-1,0"
+     */
+    LEFT(-1, 0),
+
+    /**
+     * .
+     * 
+     * @value "1,0"
+     */
+    RIGHT(1, 0);
+
+    private int x;
+    private int y;
+
+    /**
+     * Player direction, only one character.
+     */
+
+    private Direction(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * Getter Position x.
+     * 
+     * @return positionX
+     */
+   
+    public int getX()
+    {
+        return x;
+    }
+    
+    /**
+     * Getter Position y.
+     * 
+     * @return positionY
+     */
+    public int getY()
+    {
+        return y;
+    }
 }
