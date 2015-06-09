5fc04291e120d7164fb43f7a8b5f81dde230740a
kelemen <nicolas.kelemen0@gmail.com>
2015-06-09 14:11:03 +0200

    Modification of the VisionField class.

--- src/fr/iutvalence/ardechois/stealthgameproject/model/VisionField.java
+++ src/fr/iutvalence/ardechois/stealthgameproject/model/VisionField.java
@@ -1,30 +1,113 @@
 package fr.iutvalence.ardechois.stealthgameproject.model;
 
+import fr.iutvalence.ardechois.stealthgameproject.exceptions.InvalidPositionException;
+
 //TODO Javadoc ;)
 public class VisionField
 {
+	//Constants
+	/**
+	 * The default vision field width.
+	 */
+	public final static int DEFAULT_WIDTH = 5;
+	
+	/**
+	 * The default vision field height;
+	 */
+	public final static int DEFAULT_HEIGHT = 3;
+	
+	//Attributes
+	/**
+	 * The vision field width
+	 */
+	private int width;
+
 	/**
 	 * The vision field height
 	 */
 	private int height;
 
 	/**
-	 * The vision field length
+	 * The vision field position
+	 * 
+	 * @see Position
 	 */
-	private int lenght;
+	private Position position;
 
-	// TODO Javadoc ;)
-	public void rectangle(int lenght, int height)
+	/**
+	 * Constructor with a position parameter.
+	 * 
+	 * @param position
+	 * 			The vision field position.
+	 */
+	public VisionField(Position position)
 	{
-		this.height = height;
-		this.lenght = lenght;
+		this.width = DEFAULT_WIDTH;
+		this.height = DEFAULT_HEIGHT;
+		this.position = position;
 	}
-
-	// TODO Javadoc ;)
-	public VisionField()
+	
+	/**
+	 * Constructor with width, height and position parameters.
+	 * 
+	 * @param width
+	 * 			The vision field width.
+	 * @param height
+	 * 			The vision field height.
+	 * @param position
+	 * 			The vision field position.
+	 */
+	public VisionField(int width, int height, Position position)
 	{
-		final int Lenght;
-		final int height;
+		this.width = width;
+		this.height = height;
+		this.position = position;
+	}
+	
+	/**
+	 * Allow to rotate the vision field.
+	 */
+	private void rotate()
+	{
+		int widthTemp = this.width;
+		this.width = this.height;
+		this.height = widthTemp;
+	}
+	
+	/**
+	 * Allow to move the vision field.
+	 * 
+	 * @param rotate
+	 * 			true if a vision field rotation is needed.
+	 */
+	public void move(Direction direction, boolean rotate, Map map) throws InvalidPositionException
+	{
+		if (rotate)
+			this.rotate();
+		
+			switch (direction)
+			{
+			case UP:
+				if (this.position.getY() - 1 < 0)
+					throw new InvalidPositionException();
+				break;
+			case DOWN:
+				if (this.position.getY() + 1 >= map.getMapHeight())
+					throw new InvalidPositionException();
+				break;
+			case LEFT:
+				if (this.position.getX() - 1 < 0)
+					throw new InvalidPositionException();
+				break;
+			case RIGHT:
+				if (this.position.getX() + 1 >= map.getMapWidth())
+					throw new InvalidPositionException();
+				break;
+			}
+			
+			if(map.getBlock(new Position(this.position.getX() + direction.getX(), this.position.getY()+direction.getY())) != Blocks.WALL)
+				this.position.move(direction);
+		
 	}
 
 }
